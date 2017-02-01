f7b9016a6254c5e896abe1e08666bd6a0aa42460
Amoshalt <tardy.m02@gmail.com>
2017-02-01 02:46:26 +0100

    Affichage de tous les cocktails

--- CocktailsApp/CocktailsApp/Form1.Designer.cs
+++ CocktailsApp/CocktailsApp/Form1.Designer.cs
@@ -2,15 +2,17 @@
 {
     partial class Form1
     {
+        private isi_projet2_tardymartial_remondvictorEntities bdcockent;
         /// <summary>
-        /// Variable nécessaire au concepteur.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        
         /// <summary>
-        /// Nettoyage des ressources utilisées.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +22,62 @@
             base.Dispose(disposing);
         }
 
-        #region Code généré par le Concepteur Windows Form
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
-        /// le contenu de cette méthode avec l'éditeur de code.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cocktailsGridView = new System.Windows.Forms.DataGridView();
+            this.cocktailsTB = new System.Windows.Forms.TextBox();
+            this.cocktailsLB = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.cocktailsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cocktailsGridView
+            // 
+            this.cocktailsGridView.Location = new System.Drawing.Point(102, 12);
+            this.cocktailsGridView.Name = "cocktailsGridView";
+            this.cocktailsGridView.Size = new System.Drawing.Size(240, 150);
+            this.cocktailsGridView.TabIndex = 3;
+            // 
+            // cocktailsTB
+            // 
+            this.cocktailsTB.Location = new System.Drawing.Point(141, 206);
+            this.cocktailsTB.Name = "cocktailsTB";
+            this.cocktailsTB.Size = new System.Drawing.Size(200, 20);
+            this.cocktailsTB.TabIndex = 4;
+            // 
+            // cocktailsLB
+            // 
+            this.cocktailsLB.AutoSize = true;
+            this.cocktailsLB.Location = new System.Drawing.Point(152, 251);
+            this.cocktailsLB.Name = "cocktailsLB";
+            this.cocktailsLB.Size = new System.Drawing.Size(0, 13);
+            this.cocktailsLB.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 395);
+            this.Controls.Add(this.cocktailsLB);
+            this.Controls.Add(this.cocktailsTB);
+            this.Controls.Add(this.cocktailsGridView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cocktailsGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView cocktailsGridView;
+        private System.Windows.Forms.TextBox cocktailsTB;
+        private System.Windows.Forms.Label cocktailsLB;
     }
 }
-
---
+++ CocktailsApp/CocktailsApp/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Objects;
+using System.Data.Objects.DataClasses;
+using System.Data.EntityClient;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace CocktailsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fermer1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            //Initialize the ObjectContext
+            
+            bdcockent = new isi_projet2_tardymartial_remondvictorEntities();
+            var objectContext = ((IObjectContextAdapter)bdcockent).ObjectContext;
+            var set = objectContext.CreateObjectSet<cocktail>();
+            try
+            {
+                var cocktailQuery = from d in set
+                                                  select d.NOM_COCKTAIL;
+                
+                cocktailsGridView.ColumnCount = 1;
+                cocktailsGridView.Columns[0].Name = "Nom";
+                var repNomCock = ((ObjectQuery)cocktailQuery).Execute(MergeOption.AppendOnly);
+                foreach(string nom in repNomCock)
+                {
+                    cocktailsGridView.Rows.Add(nom);
+                    cocktailsTB.AppendText(" " + nom + "\n");
+                    cocktailsLB.Text = cocktailsLB.Text + nom + " \n";
+                }
+                                
+                cocktailsGridView.AllowUserToDeleteRows = false;
+                cocktailsGridView.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+    }
+}
