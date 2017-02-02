ddc78e80fe431bc8ff0c2c8a6f1f4527f3fc648a
p1409050 <victor.remond@etu.univ-lyon1.fr>
2017-02-02 16:19:01 +0100

    Recherche++

--- CocktailsApp/CocktailsApp/Recherche.cs
+++ CocktailsApp/CocktailsApp/Recherche.cs
@@ -29,12 +29,36 @@ namespace CocktailsApp
         protected void Initialisation(Persistance p)
         {
             m_pers = p;
-            ArrayList softs = m_pers.getSoft();
+
+            //Initialisation des softs
+            ArrayList softs = m_pers.getSofts();
+            m_softs = new string[softs.Count];
+            m_softOK = new bool[softs.Count];
+            for(int i = 0; i < softs.Count; i++)
+            {
+                m_softs[i] = (string)softs[i];
+                m_softOK[i] = false;
+            }
+
+            //Initialisation des alcools
+            ArrayList alcools = m_pers.getAlccols();
+            m_alcools = new string[alcools.Count];
+            m_alcoolOK = new bool[alcools.Count];
+            for (int i = 0; i < alcools.Count; i++)
+            {
+                m_alcools[i] = (string)alcools[i];
+                m_alcoolOK[i] = false;
+            }
         }
 
         public string[] NomSoft()
         {
             return m_softs;
+        }
+
+        public void ModifSoft(int index)
+        {
+            m_softOK[index] = !m_softOK[index];
         }
     }
 }
