e312614c98d0415c5e4b53a15fc2b6e606709588
p1409050 <victor.remond@etu.univ-lyon1.fr>
2017-02-05 14:37:32 +0100

    Modifs

--- CocktailsApp/CocktailsApp/Recherche.cs
+++ CocktailsApp/CocktailsApp/Recherche.cs
@@ -66,38 +66,45 @@ namespace CocktailsApp
                 m_alcoolOK[i] = false;
         }
 
+        //Retourne la liste des booleens representant la disponnibilite ou non d'un soft
         public bool[] ListeSofts()
         {
             return m_softOK;
         }
 
+        //Retourne la liste des booleens representant la disponnibilite ou non d'un alcool
         public bool[] ListeAlcools()
         {
             return m_alcoolOK;
         }
 
+        //Retourne tous les noms des softs
         public string[] NomSoft()
         {
             return m_softs;
         }
 
+        //Retourne tous les noms des alcools
         public string[] NomAlcool()
         {
             return m_alcools;
         }
 
+        //Modifie la disponibilite d'un soft
         public void ModifSoft(int index)
         {
             if ((index >= 0) && (index < m_softOK.Length))
                 m_softOK[index] = !m_softOK[index];
         }
 
+        //Modifie la disponibilite d'un alcool
         public void ModifAlcool(int index)
         {
             if ((index >= 0) && (index < m_alcoolOK.Length))
                 m_alcoolOK[index] = !m_alcoolOK[index];
         }
 
+        //Lance la recherche
         public ArrayList Valider()
         {
             ArrayList listeS = new ArrayList(), listeA = new ArrayList();
@@ -112,6 +119,39 @@ namespace CocktailsApp
                     listeA.Add(m_alcoolID[i]);
             }
             return m_persistance.RechercheComposee(listeS, listeA);
+        }
+
+        /* Retourne un objet de type ArrayList contenant des ArrayList representant les alcools contenus dans le cocktail
+         * Contenu des sous-ArrayList :
+         *  > NOM_ALCOOL
+         *  > DEGRE
+         *  > QUANTITE (mL)
+         */
+        public ArrayList ListeAlcool(cocktail c)
+        {
+            ArrayList liste = new ArrayList();
+            ArrayList lAlc = m_persistance.getAlcoolsCocktail(c.NUM_COCKTAIL);
+            ArrayList lIng = m_persistance.getIngredientsAlcool(c.NUM_COCKTAIL);
+            for(int i = 0; i < lAlc.Count; i++)
+            {
+                ArrayList temp = new ArrayList();
+                temp.Add(((alcool)lAlc[i]).NOM_ALCOOL);
+                temp.Add(((alcool)lAlc[i]).DEGRE);
+                int q = ((ingredientalcool)lIng[i]).QUANTITE * c.TAILLE / 100;
+                temp.Add(q);
+            }
+            return liste;
+        }
+
+        //Retourne les instructions necessaires a la fabrication du cocktail passe en parametre
+        public string[] Instructions(cocktail c)
+        {
+            ArrayList liste = m_persistance.getEtapes(c.NUM_COCKTAIL);
+            string[] tab = new string[liste.Count];
+            /*
+            for (int i = 0; i < liste.Count; i++)
+                tab[i] = ((etaperecette)liste[i]).;
+                */
         }
     }
 }
